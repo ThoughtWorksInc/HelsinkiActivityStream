453921ed97da229637d1bf62556d7e0c66593380
Ra-el Peters <rpeters@thoughtworks.com>
2015-09-03 09:59:47 +0100

    RP+DM | 181 | added shebang to pre_push.sh to make it executable from other shells

--- pre_push.sh
+++ pre_push.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 pip install -q -r requirements.txt
 pip install -q -r requirements_for_tests.txt
 ./venv/bin/nosetests && ./venv/bin/pep8 --exclude=tests/data openahjo_activity_streams tests
