458cfce9449e24c22b78495e444fd2cfaa2f66b7
Christoph Wong <chwong@thoughtworks.com>
2015-10-05 16:48:48 +0100

    AW + CW | 225 | make scrape_to_coracle look like a WSGI app

--- openahjo_activity_streams/scrape_to_coracle.py
+++ openahjo_activity_streams/scrape_to_coracle.py
@@ -1,11 +1,15 @@
 import logging
 import os
+import flask
 from openahjo_activity_streams import convert
 from openahjo_activity_streams.scheduler import Clock, stop_after
 from openahjo_activity_streams.scheduler import Scheduler
 from openahjo_activity_streams.scrape_and_push import scraper, pusher, scrape_and_push
 
-if __name__ == "__main__":
+
+def create_scheduler():
+    logging.basicConfig(level=logging.INFO)
+    application = flask.Flask(__name__, instance_path=os.environ['INSTANCE_PATH'])
     coracle_timestamp_endpoint = os.environ['CORACLE_TIMESTAMP_ENDPOINT']
     coracle_post_activity_endpoint = os.environ['CORACLE_POST_ACTIVITY_ENDPOINT']
     openahjo_endpoint = os.environ['OPENAHJO_ENDPOINT']
@@ -21,3 +25,10 @@ if __name__ == "__main__":
     clock = Clock()
     s = Scheduler(interval=1, clock=clock, stop_when=stop_after(clock, 10), event=scrape_and_push_event_loop)
     s.start()
+
+    return application
+
+
+application = create_scheduler()
+if __name__ == '__main__':
+    application.run()
