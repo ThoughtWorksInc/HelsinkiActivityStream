df499a0081461442ecd0d8f8f01be9c89699dddf
Christoph Wong <chwong@thoughtworks.com>
2015-10-05 13:43:09 +0100

    CW + AW | 225 | fix linter problem

--- openahjo_activity_streams/wsgi.py
+++ openahjo_activity_streams/wsgi.py
@@ -3,10 +3,11 @@ import os
 
 from openahjo_activity_streams.server import app as application
 
-virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
+virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR', '.'), 'virtenv')
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
+
     httpd = make_server('localhost', 8051, application)
     # Wait for a single request, serve it and quit.
     httpd.serve_forever()
