d1093fef6317c94b2c6570e72a63659bc0f66f65
Christoph Wong <chwong@thoughtworks.com>
2015-10-08 18:07:31 +0100

    CW + AW | 225 | deploy script to use env and run python as a background job

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-ssh vagrant@192.168.50.55 "python3 -m openahjo_activity_streams.scrape_to_coracle"
+ssh $USER@$IP_ADDRESS <<EOF
+cd /var/helsinkiAS/
+
+CORACLE_TIMESTAMP_ENDPOINT=http://coracle.herokuapp.com/latest-published-timestamp \
+OPENAHJO_ENDPOINT=http://dev.hel.fi/paatokset/v1/agenda_item/ \
+CORACLE_POST_ACTIVITY_ENDPOINT=http://coracle.herokuapp.com/activities \
+BEARER_TOKEN=$BEARER_TOKEN \
+nohup python3 -m openahjo_activity_streams.scrape_to_coracle > helsinkiAS.log 2> helsinkiAS.err&
+EOF
