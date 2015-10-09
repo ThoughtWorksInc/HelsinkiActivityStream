689f312c023334ec6a07a8f60ac93b0a34da0195
Christoph Wong <chwong@thoughtworks.com>
2015-10-09 14:47:29 +0100

    CW + AW | 225 | provision box with python3 docker image and add deploy script that uses docker

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 ssh $USER@$IP_ADDRESS <<EOF
-cd /var/helsinkiAS/
-
-CORACLE_TIMESTAMP_ENDPOINT=http://coracle.herokuapp.com/latest-published-timestamp \
-OPENAHJO_ENDPOINT=http://dev.hel.fi/paatokset/v1/agenda_item/ \
-CORACLE_POST_ACTIVITY_ENDPOINT=http://coracle.herokuapp.com/activities \
-BEARER_TOKEN=$BEARER_TOKEN \
-nohup python3 -m openahjo_activity_streams.scrape_to_coracle > helsinkiAS.log 2> helsinkiAS.err&
+  sudo docker stop helsinki || echo 'Failed to stop helsinki container'
+  sudo docker rm helsinki || echo 'Failed to remove helsinki container'
+sudo docker run -d -v /var/helsinkiAS/:/var/helsinkiAS \
+                    --env-file=/var/helsinkiAS/helsinkiAS.env \
+                    --name helsinki \
+                    python:3.4.3 \
+                    bash -c 'pip install -r /var/helsinkiAS/requirements.txt && \
+                    python -m openahjo_activity_streams.scrape_to_coracle'
 EOF
