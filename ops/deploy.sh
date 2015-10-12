790e4cf2f42d456b8c61568776158c46fff8dff0
Christoph Wong <chwong@thoughtworks.com>
2015-10-12 10:49:06 +0100

    CW + AW | 225 | add scp stage in deploy script to update code on server

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+ssh $USER@$IP_ADDRESS "sudo mkdir -p /var/helsinkiAS"
+scp -r ../openahjo_activity_streams $USER@$IP_ADDRESS:/var/helsinkiAS
+scp ../requirements.txt $USER@$IP_ADDRESS:/var/helsinkiAS
+scp helsinkiAS.env $USER@$IP_ADDRESS:/var/helsinkiAS
 
 ssh $USER@$IP_ADDRESS <<EOF
   sudo docker stop helsinki || echo 'Failed to stop helsinki container'
   sudo docker rm helsinki || echo 'Failed to remove helsinki container'
-sudo docker run -d -v /var/helsinkiAS/:/var/helsinkiAS \
+  sudo docker run -d -v /var/helsinkiAS/:/var/helsinkiAS \
                     --env-file=/var/helsinkiAS/helsinkiAS.env \
                     --name helsinki \
                     python:3.4.3 \
